1e874587bac80b76ec0157fd2da4261335bce4cf
debabrata-97 <debabrataszr@gmail.com>
2023-03-10 20:33:09 +0530

    Added : code for Stack & Queue Problem

---
+++ src/com/bridgelabz/MyStack.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+public class MyStack<K> {
+    LinkedList myLinkedList = new LinkedList();
+
+    public void push(INode element) {
+        myLinkedList.addToFront(element);
+    }
+    public void pushQueue(INode element) {
+        myLinkedList.appendToLast(element);
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+    public INode pop() {
+        return myLinkedList.popFirstNode();
+    }
+
+    public INode dequeue() {
+        return myLinkedList.popFirstNode();
+    }
+    public void printStack() {
+        myLinkedList.printMyNodes();
+    }
+
+}
+
---
+++ src/com/bridgelabz/Stack.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class Stack {
+    public static void main(String[] args) {
+
+
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+
+        INode topElement = myStack.peak();
+        System.out.println("Top most element = " + topElement.getKey());
+        myStack.pop();
+
+        myStack.printStack();
+    }
+}
+
